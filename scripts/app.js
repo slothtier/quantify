81d27fe8479ddd42c6bb0633b9e8dcdb6a2613f5
Nik Holzer <nikolaus.holzer@bwinparty.com>
2014-09-12 14:48:26 +0200

    revolving

---
+++ scripts/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name quantifyApp
+ * @description
+ * # quantifyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('quantifyApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngRoute',
+    'quantifyApp.controllers'
+  ])
+  .run(function($rootScope, $location) {
+     //console.log($location.$$hash);
+        if($location.$$hash.search(/([A-Za-z0-9_-]{155})/ig) > -1){
+            $rootScope.location = $location.$$hash;
+            $location.path('/quantify');
+            $location.url($location.path());
+            //console.log($rootScope.location);
+            //console.log($location.$$hash);
+            //console.log("authenticated!");
+        }
+
+  })
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/quantify', {
+            templateUrl: '/views/quantify.html',
+            controller: 'QuantifyCtrl'
+            })
+            .when('/auth', {
+                templateUrl: '/views/auth.html',
+                controller: 'AuthCtrl'
+            })
+            .otherwise({
+                redirectTo: '/auth'
+            });
+
+        $locationProvider.html5Mode(true);
+  }]);
